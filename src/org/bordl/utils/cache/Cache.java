b416f2fa3c6f3aa3f143d49377f1ea0e9c044fe4
Moxa <Moxa@Moxa-PC>
2012-11-24 17:15:08 +0400

    rewrite cache

--- src/org/bordl/utils/cache/Cache.java
+++ src/org/bordl/utils/cache/Cache.java
@@ -17,105 +17,108 @@ import java.util.logging.Logger;
  *
  * @author Moxa
  */
-public abstract class Cache<K, V> {
+public class Cache<K, V> {
 
-    private final ConcurrentHashMap<K, Future<V>> cache = new ConcurrentHashMap<K, Future<V>>();
+    private final ConcurrentHashMap<K, Holder<V>> cache = new ConcurrentHashMap<K, Holder<V>>();
     private final ConcurrentLinkedQueue<Entry<K, Long>> timings = new ConcurrentLinkedQueue<Entry<K, Long>>();
     private long lifetime;
+    private Computable<K, V> computable;
 
-    public Cache(long lifetime, long checkPeriod) {
-        this.lifetime = lifetime;
-        new CacheControl(checkPeriod).start();
+    public Cache(long lifetimeSec, long checkPeriodSec, Computable<K, V> computable) {
+        this.lifetime = lifetimeSec * 1000;
+        this.computable = computable;
+        new CacheControl(checkPeriodSec).start();
+    }
+
+    public Cache(long lifetimeSec, long checkPeriodSec) {
+        this(lifetimeSec, checkPeriodSec, null);
+    }
+
+    public Cache(long lifetimeSec, Computable<K, V> computable) {
+        this(lifetimeSec, lifetimeSec / 2, computable);
+    }
+
+    public Cache(long lifetimeSec) {
+        this(lifetimeSec, lifetimeSec / 2);
     }
 
     public V get(K k) {
-        try {
-            return getFromCache(k);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Cache.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return get(k, 5);
+        return getFromCache(k, computable);
     }
 
-    public V get(K k, int retries) {
-        try {
-            return getFromCache(k);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Cache.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if (retries > 0) {
-            return get(k, retries - 1);
-        } else {
-            return null;
-        }
+    public V get(K k, Computable<K, V> computable) {
+        return getFromCache(k, computable);
     }
 
-    abstract protected V compute(K k);
+    public static interface Computable<K, V> {
 
-    private V getFromCache(final K key) throws InterruptedException {
-        while (true) {
-            Future<V> f = cache.get(key);
-            if (f == null) {
-                Callable<V> eval = new Callable<V>() {
+        public V compute(K k);
+    }
 
-                    @Override
-                    public V call() {
-                        return compute(key);
+    public void clear() {
+        timings.clear();
+        cache.clear();
+    }
+
+    private class Holder<V> {
+
+        protected V v;
+        protected boolean done = false;
+
+        public Holder() {
+        }
+
+        public Holder(V v) {
+            this.v = v;
+            done = true;
+        }
+
+        public V get() {
+            if (!done) {
+                synchronized (this) {
+                    while (!done) {
+                        try {
+                            this.wait();
+                        } catch (InterruptedException ex) {
+                            //ignore
+                        }
                     }
-                };
-                FutureTask<V> ft = new FutureTask<V>(eval);
-                f = cache.putIfAbsent(key, ft);
-                if (f == null) {
-                    f = ft;
-                    ft.run();
-                    updateTimingCache(key);
                 }
             }
-            try {
-                return f.get();
-            } catch (CancellationException e) {
-                cache.remove(key, f);
-            } catch (ExecutionException e) {
-                throw new RuntimeException(e.getCause());
+            return v;
+        }
+
+        public void run(Computable<K, V> c, K k) {
+            v = c.compute(k);
+            synchronized (this) {
+                done = true;
+                this.notifyAll();
             }
         }
+    }
+
+    private V getFromCache(final K key, Computable<K, V> c) {
+        Holder<V> f = cache.get(key);
+        if (f == null) {
+            Holder<V> ft = new Holder<V>();
+            f = cache.putIfAbsent(key, ft);
+            if (f == null) {
+                f = ft;
+                ft.run(c, key);
+                updateTimingCache(key);
+            }
+        }
+        return f.get();
     }
 
     public void put(final K key, final V value) {
-        cache.put(key, new Future<V>() {
-
-            @Override
-            public boolean cancel(boolean mayInterruptIfRunning) {
-                return false;
-            }
-
-            @Override
-            public boolean isCancelled() {
-                return false;
-            }
-
-            @Override
-            public boolean isDone() {
-                return true;
-            }
-
-            @Override
-            public V get() throws InterruptedException, ExecutionException {
-                return value;
-            }
-
-            @Override
-            public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-                return value;
-            }
-        });
+        cache.put(key, new Holder<V>(value));
         updateTimingCache(key);
     }
 
     private void updateTimingCache(final K key) {
         final Long timing = lifetime + System.currentTimeMillis();
         timings.add(new Entry<K, Long>() {
-
             @Override
             public K getKey() {
                 return key;
@@ -138,30 +141,34 @@ public abstract class Cache<K, V> {
     }
 
     public boolean contains(K key) {
-        return cache.contains(key);
+        return cache.containsKey(key);
     }
 
     private class CacheControl extends Thread {
 
         private long checkPeriod;
+        private boolean enabled = true;
 
-        public CacheControl(long checkPeriod) {
+        public CacheControl(long checkPeriodSec) {
             setDaemon(true);
-            this.checkPeriod = checkPeriod;
+            this.checkPeriod = checkPeriodSec * 1000;
+            if (checkPeriodSec <= 0) {
+                enabled = false;
+            }
         }
 
         @Override
         public void run() {
             Entry<K, Long> entry = null;
-            while (true) {
+            while (enabled) {
                 try {
                     Thread.sleep(checkPeriod);
                 } catch (InterruptedException ex) {
-                    Logger.getLogger(Cache.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(Cache1.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 Long time = System.currentTimeMillis();
                 while ((entry = timings.peek()) != null && entry.getValue().compareTo(time) < 0) {
-                    cache.remove(timings.poll().getKey()).cancel(true);
+                    cache.remove(timings.poll().getKey());
                 }
             }
         }
