3b32deea29fa7e3a39179ec2705f9a9f682f8c19
moxa <mbobrutskov@lokata.ru>
2013-01-22 17:53:32 +0400

    small fix

--- src/org/bordl/utils/Pair.java
+++ src/org/bordl/utils/Pair.java
@@ -12,4 +12,24 @@ public class Pair<K, V> {
         this.key = key;
         this.value = value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair pair = (Pair) o;
+
+        if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
+        if (value != null ? !value.equals(pair.value) : pair.value != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key != null ? key.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
 }
