f8ca626bd0c58da7758ad9723623712a88b1813e
MikhailBobrutskov <mbobrutskov@lokata.ru>
2013-05-15 21:48:09 +0400

    small fix

--- src/org/bordl/utils/MapUtils.java
+++ src/org/bordl/utils/MapUtils.java
@@ -7,93 +7,92 @@ package org.bordl.utils;
 import java.util.Map;
 
 /**
- *
  * @author Moxa
  */
 public class MapUtils {
 
     public static Long getLong(Map map, String key) {
-        if (map.containsKey(key)) {
-            Object ob = map.get(key);
-            if (ob instanceof Long) {
-                return (Long) ob;
-            }
-            if (ob instanceof String) {
-                try {
-                    return Long.parseLong(ob.toString());
-                } catch (NumberFormatException ex) {
-                }
+        Object ob = map.get(key);
+        if (ob == null)
+            return null;
+        if (ob instanceof Long) {
+            return (Long) ob;
+        }
+        if (ob instanceof String) {
+            try {
+                return Long.parseLong(ob.toString());
+            } catch (NumberFormatException ignore) {
             }
         }
         return null;
     }
 
     public static Integer getInteger(Map map, String key) {
-        if (map.containsKey(key)) {
-            Object ob = map.get(key);
-            if (ob instanceof Integer) {
-                return (Integer) ob;
-            }
-            if (ob instanceof String) {
-                try {
-                    return Integer.parseInt(ob.toString());
-                } catch (NumberFormatException ex) {
-                }
+        Object ob = map.get(key);
+        if (ob == null)
+            return null;
+        if (ob instanceof Integer) {
+            return (Integer) ob;
+        }
+        if (ob instanceof String) {
+            try {
+                return Integer.parseInt(ob.toString());
+            } catch (NumberFormatException ignore) {
             }
         }
         return null;
     }
 
     public static Double getDouble(Map map, String key) {
-        if (map.containsKey(key)) {
-            Object ob = map.get(key);
-            if (ob instanceof Double) {
-                return (Double) ob;
-            }
-            if (ob instanceof String) {
-                try {
-                    return Double.parseDouble(ob.toString());
-                } catch (NumberFormatException ex) {
-                }
+        Object ob = map.get(key);
+        if (ob == null)
+            return null;
+        if (ob instanceof Double) {
+            return (Double) ob;
+        }
+        if (ob instanceof String) {
+            try {
+                return Double.parseDouble(ob.toString());
+            } catch (NumberFormatException ignore) {
             }
         }
         return null;
     }
 
     public static Boolean getBoolean(Map map, String key) {
-        if (map.containsKey(key)) {
-            Object ob = map.get(key);
-            if (ob instanceof Boolean) {
-                return (Boolean) ob;
-            }
-            if (ob instanceof String) {
-                return Boolean.parseBoolean(ob.toString());
-            }
+        Object ob = map.get(key);
+        if (ob == null)
+            return null;
+        if (ob instanceof Boolean) {
+            return (Boolean) ob;
+        }
+        if (ob instanceof String) {
+            return Boolean.parseBoolean(ob.toString());
         }
         return null;
     }
 
     public static Float getFloat(Map map, String key) {
-        if (map.containsKey(key)) {
-            Object ob = map.get(key);
-            if (ob instanceof Float) {
-                return (Float) ob;
-            }
-            if (ob instanceof String) {
-                try {
-                    return Float.parseFloat(ob.toString());
-                } catch (NumberFormatException ex) {
-                }
+        Object ob = map.get(key);
+        if (ob == null)
+            return null;
+        if (ob instanceof Float) {
+            return (Float) ob;
+        }
+        if (ob instanceof String) {
+            try {
+                return Float.parseFloat(ob.toString());
+            } catch (NumberFormatException ignore) {
             }
         }
         return null;
     }
 
     public static String getString(Map map, String key) {
-        if (map.containsKey(key)) {
-            return map.get(key).toString();
-        }
-        return null;
+        Object ob = map.get(key);
+        if (ob == null)
+            return null;
+        return ob.toString();
     }
 
     public static long getLong(Map map, String key, long def) {
