d8c2df3c916c25743364be7040cf412b6f048a96
wizzardo <wizzardobox@gmail.com>
2014-11-12 22:10:15 +0300

    add  toChars methods

--- src/com/wizzardo/tools/misc/NumberToChars.java
+++ src/com/wizzardo/tools/misc/NumberToChars.java
@@ -7,6 +7,17 @@ package com.wizzardo.tools.misc;
 public class NumberToChars {
     final static int DELIMITER = 1000000000;
     final static long DELIMITER_LONG = 1000000000000000000l;
+    final static long DELIMITER_LONG_2X = 2147483647000000000l;
+    final static char[] THOUSAND;
+
+    static {
+        THOUSAND = new char[4000]; // because <<2 faster than *3
+        for (int i = 0; i < 1000; i++) {
+            THOUSAND[4 * i] = (char) ('0' + (i % 10));
+            THOUSAND[4 * i + 1] = (char) ('0' + ((i / 10) % 10));
+            THOUSAND[4 * i + 2] = (char) ('0' + (i / 100));
+        }
+    }
 
     public static int stringSizeOfWithMinus(long l) {
         if (l < 0) {
@@ -66,5 +77,158 @@ public class NumberToChars {
                     return 1;
             }
         }
+    }
+
+    public static int toChars(long l, char[] buf, int offset) {
+        if (l < 0) {
+            if (l == Long.MIN_VALUE) {
+                writeMinLong(buf, offset);
+                return offset + 20;
+            }
+            buf[offset++] = '-';
+            l = -l;
+        }
+
+        if (l > DELIMITER) {
+            if (l > DELIMITER_LONG_2X) {
+                int half = (int) (l / DELIMITER_LONG);
+                getChars(half, offset + 1, buf);
+
+                int center = (int) ((l - half * DELIMITER_LONG) / DELIMITER);
+                getChars(center, offset + 10, buf);
+                fillWithZeroes(buf, offset + 1, 9 - stringSizeOf(center));
+
+                half = (int) (l - half * DELIMITER_LONG - center * DELIMITER);
+                getChars(half, offset + 19, buf);
+                fillWithZeroes(buf, offset + 10, 9 - stringSizeOf(half));
+                return offset + 19;
+            }
+
+            int half = (int) (l / DELIMITER);
+            offset += stringSizeOf(half);
+            getChars(half, offset, buf);
+
+            half = (int) (l - half * 1l * DELIMITER);
+            getChars(half, offset + 9, buf);
+            fillWithZeroes(buf, offset, 9 - stringSizeOf(half));
+            return offset + 9;
+        } else {
+            int i = (int) l;
+            offset += stringSizeOf(i);
+            getChars(i, offset, buf);
+            return offset;
+        }
+    }
+
+    private static void fillWithZeroes(char[] buf, int offset, int length) {
+        for (int i = offset; i < length + offset; i++) {
+            buf[i] = '0';
+        }
+    }
+
+    /*
+    * @return new offset in char buffer
+    */
+    public static int toChars(int i, char[] buf, int offset) {
+        if (i < 0) {
+            if (i == Integer.MIN_VALUE) {
+                writeMinInteger(buf, offset);
+                return offset + 11;
+            }
+            buf[offset++] = '-';
+            i = -i;
+        }
+
+        offset += stringSizeOf(i);
+        getChars(i, offset, buf);
+        return offset;
+    }
+
+    private static void writeMinInteger(char[] buf, int offset) {
+        //-2 147 483 648
+        buf[offset++] = '-';
+        buf[offset++] = '2';
+
+        buf[offset++] = '1';
+        buf[offset++] = '4';
+        buf[offset++] = '7';
+
+        buf[offset++] = '4';
+        buf[offset++] = '8';
+        buf[offset++] = '3';
+
+        buf[offset++] = '6';
+        buf[offset++] = '4';
+        buf[offset] = '8';
+    }
+
+    private static void writeMinLong(char[] buf, int offset) {
+        //-9 223 372 036 854 775 808
+        buf[offset++] = '-';
+        buf[offset++] = '9';
+
+        buf[offset++] = '2';
+        buf[offset++] = '2';
+        buf[offset++] = '3';
+
+        buf[offset++] = '3';
+        buf[offset++] = '7';
+        buf[offset++] = '2';
+
+        buf[offset++] = '0';
+        buf[offset++] = '3';
+        buf[offset++] = '6';
+
+        buf[offset++] = '8';
+        buf[offset++] = '5';
+        buf[offset++] = '4';
+
+        buf[offset++] = '7';
+        buf[offset++] = '7';
+        buf[offset++] = '5';
+
+        buf[offset++] = '8';
+        buf[offset++] = '0';
+        buf[offset] = '8';
+    }
+
+    private static void getChars(int i, int index, char[] buf) {
+        int r;
+        if (i >= 1000) {
+            int q = i / 1000;
+            r = i - (q * 1000);
+            i = q;
+            r = r << 2;
+            buf[index - 1] = THOUSAND[r];
+            buf[index - 2] = THOUSAND[r + 1];
+            buf[index -= 3] = THOUSAND[r + 2];
+            if (i >= 1000) {
+                q = i / 1000;
+                r = i - (q * 1000);
+                i = q;
+                r = r << 2;
+                buf[index - 1] = THOUSAND[r];
+                buf[index - 2] = THOUSAND[r + 1];
+                buf[index -= 3] = THOUSAND[r + 2];
+                if (i >= 1000) {
+                    q = i / 1000;
+                    r = i - (q * 1000);
+                    i = q;
+                    r = r << 2;
+                    buf[index - 1] = THOUSAND[r];
+                    buf[index - 2] = THOUSAND[r + 1];
+                    buf[index -= 3] = THOUSAND[r + 2];
+                }
+            }
+        }
+
+        r = i << 2;
+        buf[index - 1] = THOUSAND[r];
+        if (i < 10)
+            return;
+        buf[index - 2] = THOUSAND[r + 1];
+        if (i < 100)
+            return;
+        buf[index - 3] = THOUSAND[r + 2];
     }
 }
--- test/com/wizzardo/tools/misc/NumberToCharsTest.java
+++ test/com/wizzardo/tools/misc/NumberToCharsTest.java
@@ -76,5 +76,110 @@ public class NumberToCharsTest {
         Assert.assertEquals(String.valueOf(l = -12345678901234567l).length(), stringSizeOfWithMinus(l));
         Assert.assertEquals(String.valueOf(l = -123456789012345678l).length(), stringSizeOfWithMinus(l));
         Assert.assertEquals(String.valueOf(l = -1234567890123456789l).length(), stringSizeOfWithMinus(l));
+
+        for (int j = 1; j > 0; j *= 10) {
+            Assert.assertEquals(String.valueOf(j).length(), stringSizeOf(j));
+        }
+        for (int j = -1; j < 0; j *= 10) {
+            Assert.assertEquals(String.valueOf(j).length(), stringSizeOfWithMinus(j));
+        }
+        for (long j = 1; j > 0; j *= 10) {
+            Assert.assertEquals(String.valueOf(j).length(), stringSizeOf(j));
+        }
+        for (long j = -1; j < 0; j *= 10) {
+            Assert.assertEquals(String.valueOf(j).length(), stringSizeOfWithMinus(j));
+        }
+    }
+
+    @Test
+    public void toCharsTestInteger() {
+        char[] chars = new char[11];
+        int i;
+        Assert.assertEquals(String.valueOf(i = 1), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 12), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 123), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 1234), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 12345), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 123456), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 1234567), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 12345678), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 123456789), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = 1234567890), new String(chars, 0, toChars(i, chars, 0)));
+
+        Assert.assertEquals(String.valueOf(i = -1), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -12), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -123), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -1234), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -12345), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -123456), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -1234567), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -12345678), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -123456789), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = -1234567890), new String(chars, 0, toChars(i, chars, 0)));
+
+        Assert.assertEquals(String.valueOf(i = Integer.MIN_VALUE), new String(chars, 0, toChars(i, chars, 0)));
+        Assert.assertEquals(String.valueOf(i = Integer.MAX_VALUE), new String(chars, 0, toChars(i, chars, 0)));
+
+        for (int j = 1; j > 0; j *= 10) {
+            Assert.assertEquals(String.valueOf(j), new String(chars, 0, toChars(j, chars, 0)));
+        }
+        for (int j = -1; j < 0; j *= 10) {
+            Assert.assertEquals(String.valueOf(j), new String(chars, 0, toChars(j, chars, 0)));
+        }
+    }
+
+    @Test
+    public void toCharsTestLong() {
+        char[] chars = new char[20];
+        long l;
+        Assert.assertEquals(String.valueOf(l = 1), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 12), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 123), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 1234), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 12345), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 123456), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 1234567), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 12345678), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 123456789), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 1234567890), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 12345678901l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 123456789012l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 1234567890123l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 12345678901234l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 123456789012345l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 1234567890123456l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 12345678901234567l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 123456789012345678l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = 1234567890123456789l), new String(chars, 0, toChars(l, chars, 0)));
+
+        Assert.assertEquals(String.valueOf(l = -1), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -12), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -123), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -1234), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -12345), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -123456), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -1234567), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -12345678), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -123456789), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -1234567890), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -12345678901l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -123456789012l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -1234567890123l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -12345678901234l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -123456789012345l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -1234567890123456l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -12345678901234567l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -123456789012345678l), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = -1234567890123456789l), new String(chars, 0, toChars(l, chars, 0)));
+
+        Assert.assertEquals(String.valueOf(l = Long.MAX_VALUE), new String(chars, 0, toChars(l, chars, 0)));
+        Assert.assertEquals(String.valueOf(l = Long.MIN_VALUE), new String(chars, 0, toChars(l, chars, 0)));
+
+        for (long j = 1; j > 0; j *= 10) {
+            Assert.assertEquals(String.valueOf(j), new String(chars, 0, toChars(j, chars, 0)));
+        }
+        for (long j = -1; j < 0; j *= 10) {
+            Assert.assertEquals(String.valueOf(j), new String(chars, 0, toChars(j, chars, 0)));
+        }
     }
 }
