a9258407ab8fc67b79c9cc601b533c1bbcf1448d
wizzardo <wizzardobox@gmail.com>
2014-11-12 22:20:05 +0300

    improve coverage

--- test/com/wizzardo/tools/misc/NumberToCharsTest.java
+++ test/com/wizzardo/tools/misc/NumberToCharsTest.java
@@ -14,16 +14,16 @@ public class NumberToCharsTest {
     @Test
     public void stringSizeOfTest() {
         int i;
-        Assert.assertEquals(String.valueOf(i = 1).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 12).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 123).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 1234).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 12345).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 123456).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 1234567).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 12345678).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 123456789).length(), stringSizeOf(i));
-        Assert.assertEquals(String.valueOf(i = 1234567890).length(), stringSizeOf(i));
+        Assert.assertEquals(String.valueOf(i = 1).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 12).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 123).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 1234).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 12345).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 123456).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 1234567).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 12345678).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 123456789).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(i = 1234567890).length(), stringSizeOfWithMinus(i));
 
         Assert.assertEquals(String.valueOf(i = -1).length(), stringSizeOfWithMinus(i));
         Assert.assertEquals(String.valueOf(i = -12).length(), stringSizeOfWithMinus(i));
@@ -37,25 +37,25 @@ public class NumberToCharsTest {
         Assert.assertEquals(String.valueOf(i = -1234567890).length(), stringSizeOfWithMinus(i));
 
         long l;
-        Assert.assertEquals(String.valueOf(l = 1).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 12).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 123).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 1234).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 12345).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 123456).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 1234567).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 12345678).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 123456789).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 1234567890).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 12345678901l).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 123456789012l).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 1234567890123l).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 12345678901234l).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 123456789012345l).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 1234567890123456l).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 12345678901234567l).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 123456789012345678l).length(), stringSizeOf(l));
-        Assert.assertEquals(String.valueOf(l = 1234567890123456789l).length(), stringSizeOf(l));
+        Assert.assertEquals(String.valueOf(l = 1).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 12).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 123).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 1234).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 12345).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 123456).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 1234567).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 12345678).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 123456789).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 1234567890).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 12345678901l).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 123456789012l).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 1234567890123l).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 12345678901234l).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 123456789012345l).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 1234567890123456l).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 12345678901234567l).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 123456789012345678l).length(), stringSizeOfWithMinus(l));
+        Assert.assertEquals(String.valueOf(l = 1234567890123456789l).length(), stringSizeOfWithMinus(l));
 
         Assert.assertEquals(String.valueOf(l = -1).length(), stringSizeOfWithMinus(l));
         Assert.assertEquals(String.valueOf(l = -12).length(), stringSizeOfWithMinus(l));
@@ -78,17 +78,20 @@ public class NumberToCharsTest {
         Assert.assertEquals(String.valueOf(l = -1234567890123456789l).length(), stringSizeOfWithMinus(l));
 
         for (int j = 1; j > 0; j *= 10) {
-            Assert.assertEquals(String.valueOf(j).length(), stringSizeOf(j));
+            Assert.assertEquals(String.valueOf(j).length(), stringSizeOfWithMinus(j));
         }
         for (int j = -1; j < 0; j *= 10) {
             Assert.assertEquals(String.valueOf(j).length(), stringSizeOfWithMinus(j));
         }
         for (long j = 1; j > 0; j *= 10) {
-            Assert.assertEquals(String.valueOf(j).length(), stringSizeOf(j));
+            Assert.assertEquals(String.valueOf(j).length(), stringSizeOfWithMinus(j));
         }
         for (long j = -1; j < 0; j *= 10) {
             Assert.assertEquals(String.valueOf(j).length(), stringSizeOfWithMinus(j));
         }
+
+        Assert.assertEquals(String.valueOf(i = Integer.MIN_VALUE).length(), stringSizeOfWithMinus(i));
+        Assert.assertEquals(String.valueOf(l = Long.MIN_VALUE).length(), stringSizeOfWithMinus(l));
     }
 
     @Test
@@ -181,5 +184,10 @@ public class NumberToCharsTest {
         for (long j = -1; j < 0; j *= 10) {
             Assert.assertEquals(String.valueOf(j), new String(chars, 0, toChars(j, chars, 0)));
         }
+    }
+
+    @Test
+    public void forCoverage() {
+        Assert.assertNotNull(new NumberToChars());
     }
 }
