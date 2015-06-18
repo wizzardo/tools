a683667c34cb5395b35734442f94fa0391ed8c0f
wizzardo <wizzardobox@gmail.com>
2015-06-18 21:41:45 +0300

    add AbstractQueuedPool

--- modules/tools-misc/src/main/java/com/wizzardo/tools/misc/pool/SharedPool.java
+++ modules/tools-misc/src/main/java/com/wizzardo/tools/misc/pool/SharedPool.java
@@ -7,61 +7,12 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 /**
  * Created by wizzardo on 18.06.15.
  */
-public abstract class SharedPool<T> implements Pool<T> {
+public abstract class SharedPool<T> extends AbstractQueuedPool<T> {
 
     protected final Queue<Holder<T>> queue = new ConcurrentLinkedQueue<Holder<T>>();
 
     @Override
-    public T get() {
-        T value;
-        do {
-            Holder<T> item = queue.poll();
-            if (item == null)
-                return create();
-
-            value = item.get();
-        } while (value == null);
-
-        return value;
-    }
-
-    @Override
-    public Holder<T> holder() {
-        Holder<T> item = queue.poll();
-        if (item == null)
-            return createHolder(create());
-
-        return item;
-    }
-
-    @Override
-    public void release(T t) {
-        release(createHolder(t));
-    }
-
-    protected void release(Holder<T> holder) {
-        queue.add(holder);
-    }
-
-    protected Holder<T> createHolder(T t) {
-        return new SimpleHolder(t);
-    }
-
-    protected class SimpleHolder implements Holder<T> {
-        final T value;
-
-        public SimpleHolder(T value) {
-            this.value = value;
-        }
-
-        @Override
-        public T get() {
-            return value;
-        }
-
-        @Override
-        public void close() throws IOException {
-            release(value);
-        }
+    protected Queue<Holder<T>> queue() {
+        return queue;
     }
 }
