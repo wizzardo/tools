a8aad49e7dcbf368a1b048ef945f3eb5b906484f
wizzardo <wizzardobox@gmail.com>
2015-08-01 00:11:46 +0300

    rewrite StringReflection

--- modules/tools-reflection/src/main/java/com/wizzardo/tools/reflection/StringReflection.java
+++ modules/tools-reflection/src/main/java/com/wizzardo/tools/reflection/StringReflection.java
@@ -5,22 +5,134 @@ package com.wizzardo.tools.reflection;
  * Date: 8/8/14
  */
 public class StringReflection {
-    private static FieldReflection value;
-    private static FieldReflection hash;
-    private static FieldReflection count;
-    private static FieldReflection offset;
+
+    private static StringReflections reflections;
 
     static {
-        init();
-    }
-
-    private static void init() {
-        value = getFieldReflection(String.class, "value", true);
-        offset = getFieldReflection(String.class, "offset", false);
-        count = getFieldReflection(String.class, "count", false);
-        hash = getFieldReflection(String.class, "hash", false);
+        FieldReflection value = getFieldReflection(String.class, "value", true);
+        FieldReflection offset = getFieldReflection(String.class, "offset", false);
+        FieldReflection count = getFieldReflection(String.class, "count", false);
+        FieldReflection hash = getFieldReflection(String.class, "hash", false);
         if (hash == null)
             hash = getFieldReflection(String.class, "hashCode", true);
+
+        if (value == null || hash == null)
+            reflections = new NoStringReflections();
+        else if (offset != null && count != null)
+            reflections = new StringReflectionsJava6(value, hash, count, offset);
+        else
+            reflections = new StringReflectionsJava7(value, hash);
+    }
+
+    interface StringReflections {
+        int offset(String s);
+
+        char[] chars(String s);
+
+        String createString(char[] chars);
+
+        String createString(char[] chars, int hash);
+    }
+
+    static class StringReflectionsJava6 implements StringReflections {
+        final FieldReflection value;
+        final FieldReflection hash;
+        final FieldReflection count;
+        final FieldReflection offset;
+
+        StringReflectionsJava6(FieldReflection value, FieldReflection hash, FieldReflection count, FieldReflection offset) {
+            this.value = value;
+            this.hash = hash;
+            this.count = count;
+            this.offset = offset;
+        }
+
+        @Override
+        public int offset(String s) {
+            return offset.getInteger(s);
+        }
+
+        @Override
+        public char[] chars(String s) {
+            return (char[]) value.getObject(s);
+        }
+
+        @Override
+        public String createString(char[] chars) {
+            String s = new String();
+            value.setObject(s, chars);
+            count.setInteger(s, chars.length);
+            return s;
+        }
+
+        @Override
+        public String createString(char[] chars, int hash) {
+            String s = new String();
+            value.setObject(s, chars);
+            count.setInteger(s, chars.length);
+
+            if (hash != 0)
+                this.hash.setInteger(s, hash);
+            return s;
+        }
+    }
+
+    static class StringReflectionsJava7 implements StringReflections {
+        final FieldReflection value;
+        final FieldReflection hash;
+
+        StringReflectionsJava7(FieldReflection value, FieldReflection hash) {
+            this.value = value;
+            this.hash = hash;
+        }
+
+        @Override
+        public int offset(String s) {
+            return 0;
+        }
+
+        @Override
+        public char[] chars(String s) {
+            return (char[]) value.getObject(s);
+        }
+
+        @Override
+        public String createString(char[] chars) {
+            String s = new String();
+            value.setObject(s, chars);
+            return s;
+        }
+
+        @Override
+        public String createString(char[] chars, int hash) {
+            String s = new String();
+            value.setObject(s, chars);
+            if (hash != 0)
+                this.hash.setInteger(s, hash);
+            return s;
+        }
+    }
+
+    static class NoStringReflections implements StringReflections {
+        @Override
+        public int offset(String s) {
+            return 0;
+        }
+
+        @Override
+        public char[] chars(String s) {
+            return s.toCharArray();
+        }
+
+        @Override
+        public String createString(char[] chars) {
+            return new String(chars);
+        }
+
+        @Override
+        public String createString(char[] chars, int hash) {
+            return new String(chars);
+        }
     }
 
     private static FieldReflection getFieldReflection(Class clazz, String fieldName, boolean printStackTrace) {
@@ -34,42 +146,18 @@ public class StringReflection {
     }
 
     public static char[] chars(String s) {
-        if (value == null)
-            return s.toCharArray();
-
-        return (char[]) value.getObject(s);
+        return reflections.chars(s);
     }
 
     public static int offset(String s) {
-        if (value != null && offset != null)
-            return offset.getInteger(s);
-        return 0;
+        return reflections.offset(s);
     }
 
     public static String createString(char[] chars) {
-        if (value == null)
-            return new String(chars);
-
-        String s = new String();
-
-        value.setObject(s, chars);
-        if (count != null)
-            count.setInteger(s, chars.length);
-        return s;
+        return reflections.createString(chars);
     }
 
     public static String createString(char[] chars, int hash) {
-        if (value == null)
-            return new String(chars);
-
-        String s = new String();
-        value.setObject(s, chars);
-
-        if (hash != 0)
-            StringReflection.hash.setInteger(s, hash);
-
-        if (count != null)
-            count.setInteger(s, chars.length);
-        return s;
+        return reflections.createString(chars, hash);
     }
 }
