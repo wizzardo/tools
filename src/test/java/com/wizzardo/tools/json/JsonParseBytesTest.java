e7620b1ad5bf2c39156405ee9625ffcab2d730f2
wizzardo <wizzardobox@gmail.com>
2015-09-29 21:16:39 +0300

    add tests

--- src/test/java/com/wizzardo/tools/json/JsonParseBytesTest.java
+++ src/test/java/com/wizzardo/tools/json/JsonParseBytesTest.java
@@ -55,4 +55,31 @@ public class JsonParseBytesTest {
 
         Assert.assertEquals(123l, binder.value);
     }
+
+    @Test
+    public void parse_number_2() {
+        byte[] data = "123.1,".getBytes();
+
+        TestBinder binder = new TestBinder();
+        JsonUtils.parseNumber(binder, data, 0, data.length, new NumberParsingContext());
+
+        Assert.assertEquals(123.1, binder.value);
+    }
+
+    @Test
+    public void parse_number_3() {
+        byte[] data = "123,".getBytes();
+
+        TestBinder binder = new TestBinder();
+        JsonUtils.parseNumber(binder, data, 0, data.length, new NumberParsingContext());
+        Assert.assertEquals(123l, binder.value);
+
+        data = "1234}".getBytes();
+        JsonUtils.parseNumber(binder, data, 0, data.length, new NumberParsingContext());
+        Assert.assertEquals(1234l, binder.value);
+
+        data = "12345]".getBytes();
+        JsonUtils.parseNumber(binder, data, 0, data.length, new NumberParsingContext());
+        Assert.assertEquals(12345l, binder.value);
+    }
 }
