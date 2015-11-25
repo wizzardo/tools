b4a14862c98b052f1b05ecf36d6e5374cc99457f
wizzardo <wizzardobox@gmail.com>
2015-11-26 02:34:10 +0300

    name anonymous classes

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/lazy/LazyGrouping.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/lazy/LazyGrouping.java
@@ -9,36 +9,11 @@ public class LazyGrouping<K, T, A, B extends LazyGroup<K, T, T>> extends Abstrac
     }
 
     public <V> Lazy<V, V> flatMap(final Mapper<B, V> mapper) {
-        final Command<V, V> main = new Command<V, V>() {
-            @Override
-            protected void start() {
-                command.start();
-            }
+        final Command<V, V> continueCommand = new ContinueCommand<V>(command);
 
-            @Override
-            protected void process(V v) {
-                child.process(v);
-            }
-        };
+        new GroupCommand<B, V>(command, mapper, continueCommand);
 
-        new Command<B, B>(command) {
-            @Override
-            protected void process(B b) {
-                mapper.map(b);
-                new Command<V, V>(getLast(b.command)) {
-                    @Override
-                    protected void end() {
-                        main.process(parent.get());
-                    }
-                };
-            }
-
-            @Override
-            protected void end() {
-            }
-        };
-
-        return new Lazy<V, V>(main);
+        return new Lazy<V, V>(continueCommand);
     }
 
     @Override
@@ -49,5 +24,58 @@ public class LazyGrouping<K, T, A, B extends LazyGroup<K, T, T>> extends Abstrac
     @Override
     public LazyGrouping<K, T, B, B> each(Consumer<B> consumer) {
         return new LazyGrouping<K, T, B, B>(new Command.EachCommand<B>(command, consumer));
+    }
+
+    private class GroupCommand<B extends LazyGroup<K, T, T>, V> extends Command<B, B> {
+        private final Mapper<B, V> mapper;
+        private final Command<V, V> continueCommand;
+
+        public GroupCommand(Command<?, B> parent, Mapper<B, V> mapper, Command<V, V> continueCommand) {
+            super(parent);
+            this.mapper = mapper;
+            this.continueCommand = continueCommand;
+        }
+
+        @Override
+        protected void process(B b) {
+            mapper.map(b);
+            new ProcessOnEndCommand<V>(getLast(b.command), continueCommand);
+        }
+
+        @Override
+        protected void end() {
+        }
+    }
+
+    private static class ContinueCommand<T> extends Command<T, T> {
+        private Command<?, ?> command;
+
+        ContinueCommand(Command<?, ?> command) {
+            this.command = command;
+        }
+
+        @Override
+        protected void start() {
+            command.start();
+        }
+
+        @Override
+        protected void process(T t) {
+            child.process(t);
+        }
+    }
+
+    private static class ProcessOnEndCommand<T> extends Command<T, T> {
+        private Command<T, ?> command;
+
+        private ProcessOnEndCommand(Command<?, T> parent, Command<T, ?> command) {
+            super(parent);
+            this.command = command;
+        }
+
+        @Override
+        protected void end() {
+            command.process(parent.get());
+        }
     }
 }
