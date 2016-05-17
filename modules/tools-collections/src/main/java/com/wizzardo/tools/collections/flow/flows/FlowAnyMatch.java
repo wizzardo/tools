c200b9f70b2d6845c498033b5bd458d32ac248da
wizzardo <wizzardobox@gmail.com>
2016-05-17 22:21:12 +0200

    extends FlowProcessOnEnd in none, all and any

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowAnyMatch.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowAnyMatch.java
@@ -1,12 +1,11 @@
 package com.wizzardo.tools.collections.flow.flows;
 
 import com.wizzardo.tools.collections.flow.Filter;
-import com.wizzardo.tools.collections.flow.FlowProcessor;
 
 /**
  * Created by wizzardo on 16.04.16.
  */
-public class FlowAnyMatch<T> extends FlowProcessor<T, Boolean> {
+public class FlowAnyMatch<T> extends FlowProcessOnEnd<T, Boolean> {
     final Filter<T> filter;
     boolean result;
 
