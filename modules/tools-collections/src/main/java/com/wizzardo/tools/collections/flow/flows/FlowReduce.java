9edf17e5fd2f83e87eefcd7a78e75f15a181c5c1
wizzardo <wizzardobox@gmail.com>
2016-05-18 23:56:22 +0200

    update 'reduce'

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowReduce.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowReduce.java
@@ -7,24 +7,18 @@ import com.wizzardo.tools.collections.flow.Reducer;
  */
 public class FlowReduce<T> extends FlowProcessOnEnd<T, T> {
     private final Reducer<T> reducer;
-    private T prev;
 
     public FlowReduce(T def, Reducer<T> reducer) {
         this.reducer = reducer;
-        prev = def;
+        result = def;
     }
 
     @Override
     public void process(T t) {
-        T prev = this.prev;
+        T prev = this.result;
         if (prev == null)
-            this.prev = t;
+            this.result = t;
         else
-            this.prev = reducer.reduce(prev, t);
-    }
-
-    @Override
-    public T get() {
-        return prev;
+            this.result = reducer.reduce(prev, t);
     }
 }
