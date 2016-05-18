b61937cd0fae0b1a42a93450e6da1b97c6463738
wizzardo <wizzardobox@gmail.com>
2016-05-18 23:52:52 +0200

    simplify api for 'finish' flows

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowProcessOnEnd.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowProcessOnEnd.java
@@ -6,6 +6,9 @@ import com.wizzardo.tools.collections.flow.FlowProcessor;
  * Created by wizzardo on 11/05/16.
  */
 public abstract class FlowProcessOnEnd<A, B> extends FlowProcessor<A, B> {
+    protected boolean started = false;
+    protected B result;
+
     @Override
     protected void onEnd() {
         FlowProcessor<B, ?> child = this.child;
@@ -13,5 +16,19 @@ public abstract class FlowProcessOnEnd<A, B> extends FlowProcessor<A, B> {
             child.process(get());
             onEnd(child);
         }
+    }
+
+    @Override
+    protected void start() {
+        if (!started) {
+            started = true;
+            super.start();
+        }
+    }
+
+    @Override
+    public B get() {
+        start();
+        return result;
     }
 }
