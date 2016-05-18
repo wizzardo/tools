722ec918bc066f065dce80d765031d69a1a097ab
wizzardo <wizzardobox@gmail.com>
2016-05-19 00:04:27 +0200

    update 'max'

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowMax.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowMax.java
@@ -4,20 +4,14 @@ package com.wizzardo.tools.collections.flow.flows;
  * Created by wizzardo on 16.04.16.
  */
 public class FlowMax<A> extends FlowProcessOnEnd<A, A> {
-    private A max;
 
     public FlowMax(A def) {
-        max = def;
+        result = def;
     }
 
     @Override
     public void process(A a) {
-        if (max == null || ((Comparable<A>) max).compareTo(a) < 0)
-            max = a;
-    }
-
-    @Override
-    public A get() {
-        return max;
+        if (result == null || ((Comparable<A>) result).compareTo(a) < 0)
+            result = a;
     }
 }
