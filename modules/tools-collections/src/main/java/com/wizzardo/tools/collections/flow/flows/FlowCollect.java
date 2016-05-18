507428b0d217de9692eaa44fd6a88a1a8ac68ebf
wizzardo <wizzardobox@gmail.com>
2016-05-19 00:13:54 +0200

    update 'collect'

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowCollect.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowCollect.java
@@ -1,26 +1,18 @@
 package com.wizzardo.tools.collections.flow.flows;
 
-import com.wizzardo.tools.collections.flow.FlowProcessor;
-
 import java.util.Collection;
 
 /**
  * Created by wizzardo on 16.04.16.
  */
-public class FlowCollect<A, C extends Collection<A>> extends FlowProcessor<A, C> {
-    private final C collection;
+public class FlowCollect<A, C extends Collection<A>> extends FlowProcessOnEnd<A, C> {
 
     public FlowCollect(C collection) {
-        this.collection = collection;
+        result = collection;
     }
 
     @Override
     public void process(A a) {
-        collection.add(a);
-    }
-
-    @Override
-    public C get() {
-        return collection;
+        result.add(a);
     }
 }
