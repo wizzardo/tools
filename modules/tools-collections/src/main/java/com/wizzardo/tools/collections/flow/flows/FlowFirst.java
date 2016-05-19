d3631fbd997f433e9fb180fcb3fca9b8ed305460
wizzardo <wizzardobox@gmail.com>
2016-05-20 00:15:20 +0200

    update 'first'

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowFirst.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowFirst.java
@@ -1,26 +1,18 @@
 package com.wizzardo.tools.collections.flow.flows;
 
-import com.wizzardo.tools.collections.flow.FlowProcessor;
-
 /**
  * Created by wizzardo on 16.04.16.
  */
-public class FlowFirst<A> extends FlowProcessor<A, A> {
-    private A first;
+public class FlowFirst<A> extends FlowProcessOnEnd<A, A> {
 
     public FlowFirst(A def) {
-        first = def;
+        result = def;
     }
 
     @Override
     public void process(A a) {
-        first = a;
+        result = a;
         stop();
         onEnd();
-    }
-
-    @Override
-    public A get() {
-        return first;
     }
 }
