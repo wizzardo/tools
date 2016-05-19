12c6960762cabb8aa17d6769b0fcfa38542b325c
wizzardo <wizzardobox@gmail.com>
2016-05-20 00:17:32 +0200

    update 'collect with accumulator'

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowCollectWithAccumulator.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowCollectWithAccumulator.java
@@ -1,27 +1,20 @@
 package com.wizzardo.tools.collections.flow.flows;
 
 import com.wizzardo.tools.collections.flow.BiConsumer;
-import com.wizzardo.tools.collections.flow.FlowProcessor;
 
 /**
  * Created by wizzardo on 16.04.16.
  */
-public class FlowCollectWithAccumulator<C, T> extends FlowProcessor<T, C> {
+public class FlowCollectWithAccumulator<C, T> extends FlowProcessOnEnd<T, C> {
     private final BiConsumer<? super C, ? super T> accumulator;
-    private final C collector;
 
     public FlowCollectWithAccumulator(C collector, BiConsumer<? super C, ? super T> accumulator) {
         this.accumulator = accumulator;
-        this.collector = collector;
+        this.result = collector;
     }
 
     @Override
     public void process(T t) {
-        accumulator.consume(collector, t);
-    }
-
-    @Override
-    public C get() {
-        return collector;
+        accumulator.consume(result, t);
     }
 }
