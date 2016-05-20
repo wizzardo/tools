4a69ff79fe9d1ee01fd5d9b65e323c241d63a45b
wizzardo <wizzardobox@gmail.com>
2016-05-21 00:25:05 +0200

    update 'min with comparator'

--- modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowMinWithComparator.java
+++ modules/tools-collections/src/main/java/com/wizzardo/tools/collections/flow/flows/FlowMinWithComparator.java
@@ -7,23 +7,17 @@ import java.util.Comparator;
 /**
  * Created by wizzardo on 16.04.16.
  */
-public class FlowMinWithComparator<A> extends FlowProcessor<A, A> {
-    private A min;
+public class FlowMinWithComparator<A> extends FlowProcessOnEnd<A, A> {
     private final Comparator<? super A> comparator;
 
     public FlowMinWithComparator(A def, Comparator<? super A> comparator) {
         this.comparator = comparator;
-        min = def;
+        result = def;
     }
 
     @Override
     public void process(A a) {
-        if (min == null || comparator.compare(min, a) > 0)
-            min = a;
-    }
-
-    @Override
-    public A get() {
-        return min;
+        if (result == null || comparator.compare(result, a) > 0)
+            result = a;
     }
 }
