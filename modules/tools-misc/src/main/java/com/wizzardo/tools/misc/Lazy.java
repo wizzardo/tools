2a1fba968325a0d7bd35ff24044b5fa8f56ed4e1
wizzardo <wizzardobox@gmail.com>
2016-07-06 23:13:48 +0200

    add Lazy class

---
+++ modules/tools-misc/src/main/java/com/wizzardo/tools/misc/Lazy.java
@@ -0,0 +1,55 @@
+package com.wizzardo.tools.misc;
+
+/**
+ * Created by wizzardo on 06/07/16.
+ */
+public class Lazy<T> {
+    private volatile Holder<T> holder;
+
+    public Lazy(final Supplier<T> supplier) {
+        holder = new HolderWithSupplier<T>(new Supplier<T>() {
+            @Override
+            public T supply() {
+                synchronized (Lazy.this) {
+                    if (holder.getClass() != Holder.class)
+                        holder = new Holder<T>(supplier.supply());
+                }
+                return holder.value;
+            }
+        });
+    }
+
+    public static <T> Lazy<T> of(Supplier<T> supplier) {
+        return new Lazy<T>(supplier);
+    }
+
+    public T get() {
+        return holder.get();
+    }
+
+    static class Holder<T> {
+        final T value;
+
+        Holder(T value) {
+            this.value = value;
+        }
+
+        T get() {
+            return value;
+        }
+    }
+
+    static class HolderWithSupplier<T> extends Holder<T> {
+        Supplier<T> supplier;
+
+        HolderWithSupplier(Supplier<T> supplier) {
+            super(null);
+            this.supplier = supplier;
+        }
+
+        @Override
+        T get() {
+            return supplier.supply();
+        }
+    }
+}
