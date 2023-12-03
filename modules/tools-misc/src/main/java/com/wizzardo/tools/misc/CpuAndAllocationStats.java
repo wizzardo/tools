9b88b89d5b9b5a64476c15fbe26d2bbf53eba1e7
Mikhail Bobrutskov <wizzardobox@gmail.com>
2023-12-03 16:21:47 +0100

    added CpuAndAllocationStats

---
+++ modules/tools-misc/src/main/java/com/wizzardo/tools/misc/CpuAndAllocationStats.java
@@ -0,0 +1,76 @@
+package com.wizzardo.tools.misc;
+
+import com.sun.management.ThreadMXBean;
+
+import java.lang.management.ManagementFactory;
+
+public class CpuAndAllocationStats {
+
+    public static final boolean CPU_TIME_TRACING_SUPPORTED;
+    public static final boolean ALLOCATION_TRACING_SUPPORTED;
+    protected static final ThreadLocal<CpuAndAllocationStats> THREAD_LOCAL = ThreadLocal.withInitial(CpuAndAllocationStats::new);
+
+    static {
+        ThreadMXBean threadMXBean = (ThreadMXBean) ManagementFactory.getThreadMXBean();
+        CPU_TIME_TRACING_SUPPORTED = threadMXBean.isThreadCpuTimeSupported() && threadMXBean.isThreadCpuTimeEnabled();
+        ALLOCATION_TRACING_SUPPORTED = threadMXBean.isThreadAllocatedMemorySupported() && threadMXBean.isThreadAllocatedMemoryEnabled();
+    }
+
+    protected final long threadId;
+    protected long cpuTime;
+    protected long cpuUserTime;
+    protected long allocation;
+    protected ThreadMXBean threadMXBean;
+
+    public static CpuAndAllocationStats get() {
+        return THREAD_LOCAL.get();
+    }
+
+    public CpuAndAllocationStats() {
+        threadId = Thread.currentThread().getId();
+        threadMXBean = (com.sun.management.ThreadMXBean) ManagementFactory.getThreadMXBean();
+    }
+
+    public void reset() {
+        if (CPU_TIME_TRACING_SUPPORTED) {
+            cpuTime = getTotalCpuTime();
+            cpuUserTime = getTotalCpuUserTime();
+        }
+        if (ALLOCATION_TRACING_SUPPORTED) {
+            allocation = getTotalAllocation();
+        }
+    }
+
+    public long getCountedCpuTime() {
+        return getTotalCpuTime() - cpuTime;
+    }
+
+    public long getTotalCpuTime() {
+        if (!CPU_TIME_TRACING_SUPPORTED)
+            return 0;
+
+        return threadMXBean.getCurrentThreadCpuTime();
+    }
+
+    public long getCountedCpuUserTime() {
+        return getTotalCpuUserTime() - cpuUserTime;
+    }
+
+    public long getTotalCpuUserTime() {
+        if (!CPU_TIME_TRACING_SUPPORTED)
+            return 0;
+
+        return threadMXBean.getCurrentThreadUserTime();
+    }
+
+    public long getCountedAllocation() {
+        return getTotalAllocation() - allocation;
+    }
+
+    public long getTotalAllocation() {
+        if (!ALLOCATION_TRACING_SUPPORTED)
+            return 0;
+
+        return threadMXBean.getThreadAllocatedBytes(threadId);
+    }
+}
